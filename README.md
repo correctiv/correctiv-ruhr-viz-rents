2974393639fa6ba763a35293f015ce9cc96a0699
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-03-28 15:23:22 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,174 @@
+# correctiv-ruhr-viz-rents
+
+Break down [this](https://correctiv.github.io/rent-prices-germany/) to Ruhr/NRW
+
+## new `cmsPlugin`-instance for each viz:
+
+- bivariate map
+- scatter plot
+- comparison between *ruhr* and *munich*
+- comparison between 2012 and 2016
+
+### bivariate map
+
+**html**
+
+```html
+<figure class="figure -paragraph-width">
+  <div class="figure__container">
+    <section class="cor-viz-rents__section">
+      <img class="cor-viz-rents__bivariate-legend" src="./legend.svg">
+      <div id="rents-map"></div>
+    </section>
+    <figcaption class="figure__caption">
+      <section class="cor-viz-rents__section">
+        <div class="cor-viz-rents__selector" id="rents-map-selector"></div>
+        <div class="cor-viz-rents__infobox" id="rents-map-infobox"></div>
+        <div id="rents-map-timeline"></div>
+      </section>
+    </figcaption>
+  </div>
+</figure>
+<p class="cor-viz-rents__credits"><b>Daten:</b> <a href="">Data Source</a></p>
+```
+
+**init js**
+
+```html
+<script type="text/javascript" charset="utf-8">
+  renderRentMap();
+</script>
+```
+
+### scatter plot
+
+**html**
+
+```html
+<figure class="figure -paragraph-width">
+  <div class="figure__container">
+    <section class="cor-viz-rents__section">
+      <div id="scatter-plot"></div>
+    </section>
+    <figcaption class="figure__caption">
+      <section class="cor-viz-rents__section">
+        <div class="figure__title"><p>Einwohner</p></div>
+        <div class="cor-viz-rents__legend" id="scatter-plot-legend"></div>
+        <div class="cor-viz-rents__selector" id="scatter-plot-selector"></div>
+        <div class="cor-viz-rents__infobox" id="scatter-plot-infobox"></div>
+      </section>
+    </figcaption>
+  </div>
+</figure>
+<p class="cor-viz-rents__credits"><b>Daten:</b> <a href="">Data Source</a></p>
+```
+
+**init js**
+
+```html
+<script type="text/javascript" charset="utf-8">
+  renderScatter();
+</script>
+```
+
+### compare 'Ruhrgebiet' with 'Munich'
+
+**html**
+
+```html
+<figure class="figure -paragraph-width">
+  <div class="figure__container">
+    <section class="cor-viz-rents__section cor-viz-rents__section--small-multiples">
+      <div class="cor-viz-rents__chart" id="comparison-ruhr">
+        <p class="cor-viz-rents__title">Ruhrgebiet</p>
+      </div>
+      <div class="cor-viz-rents__chart" id="comparison-muc">
+        <p class="cor-viz-rents__title">Region um München</p>
+      </div>
+    </section>
+  </div>
+</figure>
+<p class="cor-viz-rents__annotation"><strong>Ruhrgebiet:</strong> Herne, Oberhausen, Essen, Gelsenkirchen, Duisburg, Bochum, Dortmund</p>
+<p class="cor-viz-rents__annotation"><strong>Region um München:</strong> München, Dachau, Ebersberg, Fürstenfeldbruck, München (Kreis), Starnberg</p>
+<p class="cor-viz-rents__credits"><strong>Daten:</strong> <a href="">Data Source</a></p>
+```
+
+**init js**
+
+```html
+<script type="text/javascript" charset="utf-8">
+  renderComparisonLines();
+</script>
+```
+
+
+### compare 2012 with 2016
+
+**html**
+
+```html
+<figure class="figure -paragraph-width">
+  <div class="figure__container">
+    <section class="cor-viz-rents__section cor-viz-rents__section--small-multiples">
+      <div class="cor-viz-rents__map" id="comparison-map-2012">
+        <p class="cor-viz-rents__title">2012</p>
+      </div>
+      <div class="cor-viz-rents__map" id="comparison-map-2016">
+        <p class="cor-viz-rents__title">2016</p>
+      </div>
+    </section>
+    <figcaption class="figure__caption">
+      <section class="cor-viz-rents__section">
+        <div class="figure__title"><p>Mietpreis pro&nbsp;m²</p></div>
+        <div class="cor-viz-rents__legend" id="comparison-maps-legend"></div>
+      </section>
+    </figcaption>
+  </div>
+</figure>
+<p class="cor-viz-rents__credits"><strong>Daten:</strong> <a href="">Data Source</a></p>
+```
+
+**init js**
+
+```html
+<script type="text/javascript" charset="utf-8">
+  renderComparisonMaps();
+</script>
+```
+
+
+## base visualisation setup
+
+**javascript**
+
+```
+lib/d3.v4.min.js
+lib/topojson.v2.min.js
+lib/riot.min.js
+lib/d3-playbooks.base.min.js
+lib/d3-playbooks.maps.min.js
+lib/d3-playbooks.riot-components.min.js
+cor-viz-rents.min.js
+```
+
+**styles**
+
+```
+cor-viz-rents.min.css
+```
+
+## development / build
+
+### install build deps
+
+`npm install`
+
+### build js and css for deployment
+
+`npm run build`
+
+### watch and build js and css during development
+
+`npm run dev`
+
+start a webserver to view `index.html`.
---
+++ src/main.js
@@ -0,0 +1,305 @@
+window.renderRentViz = basePath => {
+
+  d3.json(`${basePath}/data/nrw-landkreise.topo.json`, d => {
+    const geoData = topojson.feature(d, d.objects.layer)
+
+    d3.csv(`${basePath}/data/map_data.csv`, d => {
+      const data = d
+
+      if (d3.select('#rents-map').node()) renderRentMap({geoData, data})
+      // if (d3.select('#scatter-plot').node()) renderScatter(basePath)
+      if (d3.select('#comparison-ruhr').node()) renderComparisonLines(basePath)
+      if (d3.select('#comparison-map-2012')) renderComparisonMaps({geoData, data})
+
+    })
+
+  })
+
+}
+
+
+const COLORS = {
+  '11': '#e8e6f2',
+  // '11': '#d7d6e1', // FIXME ??
+  '12': '#b5d3e7',
+  '13': '#4fadd0',
+  '21': '#e5b4d9',
+  '22': '#b8b3d8',
+  '23': '#3983bb',
+  '31': '#de4fa6',
+  '32': '#b03598',
+  '33': '#2a1a8a',
+}
+
+const YEARS = [
+  '2012',
+  '2013',
+  '2014',
+  '2015',
+  '2016'
+]
+
+const cssNamespace = 'cor-viz-rents'
+d3.playbooks.defaults({cssNamespace})
+
+// initial hilight
+const initialHilight = (viz, name='Wolfsburg') => {
+  viz.ready().then(() => {
+    const initial = viz.data().find(d => d.name === name)
+    viz.hilight(initial)
+    viz.control().trigger(riot.EVT.updateSelector, initial)
+    viz.control().trigger(riot.EVT.updateInfobox, initial)
+  })
+}
+
+// bivariate map
+const renderRentMap = ({geoData, data}) => {
+
+  const M = d3.playbooks.choroplethMap({
+    width: 780,  // nrw shape ratio
+    height: 768,
+    cssNamespace,  // FIXME
+    elementId: 'rents-map',
+    data,
+    geoData,
+    responsiveSvg: true,
+    getId: f => f.properties.RS,
+    color: d => COLORS[d]
+  }).render()
+    .selector({
+      element: '#rents-map-selector',
+      getLabel: f => f.name
+    })
+    .infobox({
+      element: '#rents-map-infobox',
+      template: `
+        <dl>
+          <dt>{rent_median} € / m²</dt>
+          <dd>Miete 2016</dd>
+          <dt>{percent_change} %</dt>
+          <dd>Anstieg seit 2012</dd>
+        </dl>
+      `
+    })
+
+
+  // line chart in infobox
+  const timeLineElId = 'rents-map-timeline'
+  const timeLineEl = d3.select(`#${timeLineElId}`)
+
+  d3.playbooks.timeLineChart.defaults({
+    elementId: timeLineElId,
+    width: 150,
+    height: 200,
+    curve: d3.curveStep,
+    timeFormat: '%Y',
+    yExtent: [3.5, 15],
+    getYAxis: ({yScale}) => d3.axisRight(yScale),
+    // getXAxis: ({xScale}) => d3.axisTop(xScale),
+    showXLabel: false,
+    showYLabel: false,
+    yTickFormat: d => d > 3 ? d + ' €' : '',
+    yLabel: '€ / m²',
+    yTicks: 3,
+    xTicks: 3,
+    margin: {
+      top: 20,
+      right: 15,
+      bottom: 20,
+      left: 15
+    }
+  })
+
+  const renderTimeline = d => {
+
+    timeLineEl.selectAll('*').remove()
+
+    const data = [
+      YEARS,
+      YEARS.map(y => Number(d[y]))
+    ]
+
+    d3.playbooks.timeLineChart({
+      data,
+      color: COLORS[d.value]
+    }).render()
+
+  }
+
+  M.control().on(riot.EVT.updateInfobox, d => renderTimeline(d))
+  initialHilight(M)
+
+}
+
+
+// scatter-plot
+const renderScatter = basePath => {
+
+  const S = d3.playbooks.scatterChart({
+    elementId: 'scatter-plot',
+    dataUrl: `${basePath}/data/scatter.csv`,
+    yCol: 'rent_median',
+    xLabel: 'Einwohner pro km²',
+    yLabel: 'Mittlerer Mietpreis pro m²',
+    xCol: 'density',
+    getYDomain: () => [4, 16],
+    sizeCol: 'size',
+    sizeRange: [4, 20],
+    groupCol: 'color',
+    color: COLORS,
+    xTicks: 5,
+    yTickFormat: d => `${d} €`,
+    getLegendItems: ({data}) => {
+      const extent = d3.extent(data, d => Number(d['2016pop']))
+      const getSize = d3.scaleLinear().domain(extent).range([4, 20])
+      const population = [100000, 1000000]
+      return population.reverse().map(p => {
+        const radius = getSize(p)
+        const label = p < 1000000 ? `${p / 1000} T.` : `${p / 1000000} Mio.`
+        return {
+          'color': COLORS['11'],
+          'label': label,
+          'size': 30,
+          // 'size': radius * 2,
+          'radius': radius
+        }
+      })
+    }
+  }).render()
+
+    .selector({
+      element: '#scatter-plot-selector',
+      getLabel: d => d.name
+    })
+
+    .infobox({
+      element: '#scatter-plot-infobox',
+      template: `
+        <dl>
+          <dt>{rent_median} € / m²</dt>
+          <dd>Miete 2016</dd>
+          <dt>{percent_change} %</dt>
+          <dd>Anstieg seit 2012</dd>
+        </dl>
+      `
+    })
+
+    .legend({
+      element: '#scatter-plot-legend',
+      wrapperTemplate: '{body}',
+      itemTemplate: `
+        <div class="cor-viz-rents__scatter-legend-item">
+          <svg width="{size}" height="{size}">
+            <circle r={radius} cx=15 cy=15 style="fill:{color}"></circle>
+          </svg>
+          <span>{label}</span>
+        </div>
+      `
+    })
+
+  initialHilight(S)
+}
+
+
+// comparison charts
+const renderComparisonLines = basePath => {
+
+  d3.playbooks.multiTimeLineChart.defaults({
+    width: 250,
+    height: 300,
+    xTicks: 3,
+    yTickFormat: d => d > 0 ? d + ' €' : '',
+    yLabel: 'Mittlerer Mietpreis pro m²',
+    timeFormat: '%Y',
+    showXLabel: false,
+    curve: d3.curveStep,
+    getYDomain: () => [0, 15],
+    dataUrl: `${basePath}/data/comparison.csv`,
+    // drawExtra: ({
+    //   data,
+    //   drawedSelection,
+    //   xScale,
+    //   yScale,
+    //   g
+    // }) => {
+    //   const last = data.slice(-1)[0]
+    //   const find = d => Object.keys(last).find(k => last[k] === d)
+    //   const labels = drawedSelection.data().map(d => {
+    //     const _last = d.slice(-1)[0]
+    //     const label = find(_last)
+    //     const y = yScale(_last)
+    //     return {label, y}
+    //   })
+    //   g.selectAll('.label').data(labels).enter().append('text')
+    //     .attr('class', '.label')
+    //     .attr('x', 100)
+    //     .attr('y', d => d.y)
+    //     .text(d => d.label)
+    // }
+  })
+
+  d3.playbooks.multiTimeLineChart({
+    elementId: 'comparison-ruhr',
+    yCols:  ['Herne', 'Oberhausen', 'Essen', 'Gelsenkirchen', 'Duisburg', 'Bochum', 'Dortmund'],
+    color: COLORS[21]
+  }).render()
+
+  d3.playbooks.multiTimeLineChart({
+    elementId: 'comparison-muc',
+    yCols:  ['München', 'Dachau', 'Ebersberg', 'Fürstenfeldbruck', 'München (Kreis)', 'Starnberg'],
+    color: COLORS[23],
+    showYLabel: false
+  }).render()
+
+}
+
+
+// small multiple maps
+const renderComparisonMaps = ({geoData, data}) => {
+
+  const renderMap = (year, legend=true, otherMap=null) => {
+    const sMap = d3.playbooks.choroplethMap({
+      width: 780,  // nrw shape ratio
+      height: 768,
+      cssNamespace,  // FIXME
+      elementId: `comparison-map-${year}`,
+      yCol: year,
+      yExtent: [5, 10],
+      data,
+      geoData,
+      responsiveSvg: true,
+      getId: f => f.properties.RS,
+      hilightNode: () => {},
+      color: [COLORS['11'], COLORS['12'], COLORS['13'], COLORS['23'], COLORS['33']]
+    }).render()
+
+    if (legend) {
+      sMap.legend({
+        element: '#comparison-maps-legend',
+        itemTemplate: `<li><span style="background-color:{color}"></span>{label}&nbsp;€</li>`
+      })
+      // sMap.infobox({
+      //   template: `
+      //     <dl>
+      //       <dt>{rent_median} € / m²</dt>
+      //       <dd>Miete 2016</dd>
+      //       <dt>{2012} € / m²</dt>
+      //       <dd>Miete 2012</dd>
+      //       <dt>{percent_change} %</dt>
+      //       <dd>Anstieg seit 2012</dd>
+      //     </dl>
+      //   `
+      // })
+    }
+    // else if (otherMap) {
+    //   sMap.hilightNode(d => otherMap.hilight(d))
+    // }
+
+
+    return sMap
+  }
+
+  const map2012 = renderMap(2012)
+  renderMap(2016, false, map2012)
+
+}
